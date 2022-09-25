4f7fc4eb019205d9df545916d035cb5826fa0593
Jan-Willem Harmannij <jwharmannij@gmail.com>
2022-09-25 22:38:07 +0200

    Fix build script

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-javac -d out --enable-preview --source 19 -cp lib/gtk-4.0.jar -sourcepath src src/test/TestNotebook.java
+javac -d out --enable-preview --source 19 -cp lib/gtk4-jextract.jar -sourcepath src src/test/TestNotebook.java
