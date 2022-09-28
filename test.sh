a63afe4f594b452f551b4b75ee7f808dd6f7bde4
Jan-Willem Harmannij <jwharmannij@gmail.com>
2022-09-28 22:27:53 +0200

    Fix test script

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java --enable-preview --enable-native-access=ALL-UNNAMED -cp out:lib/gtk-4.0.jar test.TestNotebook
+java --enable-preview --enable-native-access=ALL-UNNAMED -cp out:lib/gtk4-jextract.jar test.TestNotebook
