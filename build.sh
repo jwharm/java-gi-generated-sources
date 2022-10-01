e8d1054fbd5107510a59a26e53bdb72cbd67256f
Jan-Willem Harmannij <jwharmannij@gmail.com>
2022-10-01 18:10:08 +0200

    Add module-info.java

--- build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-javac -d out --enable-preview --source 19 -cp lib/gtk4-jextract.jar -sourcepath src src/test/TestNotebook.java
--- test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-java --enable-preview --enable-native-access=ALL-UNNAMED -cp out:lib/gtk4-jextract.jar test.TestNotebook
